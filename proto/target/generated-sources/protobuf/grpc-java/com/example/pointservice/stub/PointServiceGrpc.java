010e1d028e28217dca1852c9d6719da62abcab4a
mafei <mafei.dev@gmail.com>
2023-02-12 13:58:14 +0530

    unknown

--- proto/target/generated-sources/protobuf/grpc-java/com/example/pointservice/stub/PointServiceGrpc.java
+++ proto/target/generated-sources/protobuf/grpc-java/com/example/pointservice/stub/PointServiceGrpc.java
@@ -6,7 +6,7 @@ import static io.grpc.MethodDescriptor.generateFullMethodName;
  */
 @javax.annotation.Generated(
     value = "by gRPC proto compiler (version 1.51.0)",
-    comments = "Source: point-service.proto")
+    comments = "Source: userpointservice/point-service.proto")
 @io.grpc.stub.annotations.GrpcGenerated
 public final class PointServiceGrpc {
 
